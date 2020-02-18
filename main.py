50301549f34fa3826b4b0359a5b79977c678f567
Anton Irvold <40317988+antonvdi@users.noreply.github.com>
2020-02-18 08:41:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
+#cd C:\Users\Anton\Documents\GitHub\pro_eks
+
 import pygame
 
 pygame.init()
 
+width = int(input("Width?\n"))
+height = int(input("Height?\n"))
+
+if width < 50:
+	width = 50
+elif width > 3000:
+	width = 3000
+if height < 50:
+	height = 50
+elif height > 3000:
+	height = 3000
+
+
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
-crashed = False
 
-while not crashed:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			crashed = True
+def GameLoop():
+	done = False
+	while not done:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				done = True
 
-		print(event)
+		x = 0
+		y = 0
+		RecursiveBacktracking(x, y)
 
-	pygame.display.update()
-	clock.tick(60)
+		pygame.display.flip()
+		clock.tick(60)
 
-pygame.quit()
-quit()
+def RecursiveBacktracking(x, y):
+	
+
+	
+
+
+GameLoop()
