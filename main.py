e9f60bb3af51b9360bc6564ab56b00c5cec56f45
Anton Irvold <40317988+antonvdi@users.noreply.github.com>
2020-02-18 08:22:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import pygame
 
+pygame.init()
+
+clock = pygame.time.Clock()
+
+crashed = False
+
+while not crashed:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			crashed = True
+
+		print(event)
+
+	pygame.display.update()
+	clock.tick(60)
+
+pygame.quit()
+quit()
