016eb45fe4335562e7d5fc9c72e1cb8d5ad18b3e
Anton Irvold <40317988+antonvdi@users.noreply.github.com>
2020-02-20 18:25:06 +0100

    Working with Backtracing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-#cd C:\Users\Anton\Documents\GitHub\pro_eks
-#cd C:\Users\Mathias Laptop\Documents\GitHub\pro_eks
+# cd C:\Users\Anton\Documents\GitHub\pro_eks
+# cd C:\Users\anton_mc03yx6\Documents\GitHub\pro_eks
+# cd C:\Users\Mathias Laptop\Documents\GitHub\pro_eks
 
 import pygame
 import sys
@@ -25,22 +26,24 @@ class current(object):
     	self.x = x
     	self.y = y
 
-def GameLoop():
+def GenerateMaze():
 	done = False
-
+	steps = 0
+	backwards = False
 	maze[current.x][current.y] = 1
 	
 	while not done:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				done = True
-		
-		backwards = False
 		direction_possible = [False, False, False, False]
 		CheckAdjacent(direction_possible, backwards)
 
 		if True not in direction_possible:
 			backwards = True
+			CheckAdjacent(direction_possible, backwards)
+		else:
+			backwards = False
 
 		go = False
 		while go == False:	#find en bedre måde at gøre det her på...
@@ -49,38 +52,47 @@ def GameLoop():
 				go = True
 
 		MoveRandom(random_value)
+		print(maze)
 
 		pygame.display.flip()
 		clock.tick(600)
 
+		steps += 1
+		if steps > maze_height * maze_width:
+			sys.exit()
+
 
 def CheckAdjacent(direction_possible, backwards):
 	try:
-		if maze[current.x][current.y-2] == backwards: #north
+		if current.y-2 >= 0 and maze[current.x][current.y-2] == backwards: #north
 			direction_possible[0] = True
 	except IndexError:
-		pass
+		print("IndexError")
 
 	try:
-		if maze[current.x][current.y+2] == backwards: #south
+		if current.y+2 <= maze_height and maze[current.x][current.y+2] == backwards: #south
 			direction_possible[1] = True
 	except IndexError:
-		pass
+		print("IndexError")
 
 	try:
-		if maze[current.x+2][current.y] == backwards: #east
+		if current.x+2 <= maze_width and maze[current.x+2][current.y] == backwards: #east
 			direction_possible[2] = True
 	except IndexError:
-		pass
+		print("IndexError")
 
 	try:
-		if maze[current.x-2][current.y] == backwards: #west
+		if current.x-2 >= 0 and maze[current.x-2][current.y] == backwards: #west
 			direction_possible[3] = True
 	except IndexError:
-		pass
+		print("IndexError")
+
+	if backwards == True:
+		print("Backtraced")
+	else:
+		print("Forward")
 
 	return direction_possible 
-
 
 def MoveRandom(random_value):
 	if random_value == 0:
@@ -98,7 +110,6 @@ def MoveRandom(random_value):
 	elif random_value == 3:
 		for i in range(2):
 			current.x -= 1
-			maze[current.x][current.y] = 1
-	print('moved')
+			maze[current.x][current.y] = 1	
 
-GameLoop()
+GenerateMaze()
